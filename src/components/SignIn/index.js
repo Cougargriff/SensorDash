d941e3b304eaeb7bb9a788b83224869d09dcd1b5
Griffin <cougargriff@gmail.com>
2019-04-15 09:44:59 -0700

    firebase config up

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+
+import './signin.css';
 
 const SignIn = () => (
-        <div>
-         <h1>SignIn</h1>
-        </div>
+    <div class="SignIn">
+        <Form>
+          <Form.Group controlId="email" bsSize="large">
+            <Form.Control
+              autoFocus
+              type="email"
+            />
+          </Form.Group>
+          <Form.Group controlId="password" bsSize="large">
+            <Form.Control
+              hint="password"
+              type="password"
+            />
+          </Form.Group>
+          <Button
+            block
+            bsSize="large"
+            type="submit"
+          >
+            Login
+          </Button>
+        </Form>
+      </div>
 );
 
 export default SignIn;
