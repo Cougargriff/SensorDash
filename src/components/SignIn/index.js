461796b21fbd8588086b0423521beabd0c1f43b0
Griffin <cougargriff@gmail.com>
2019-04-20 14:34:11 -0700

    SIgn in and sign out both work

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,68 +1,140 @@
 import React from 'react';
 import {
-  Form, Icon, Input, Button, Checkbox,
+    Form, Icon, Input, Button, Checkbox,
 } from 'antd';
 
 import 'antd/dist/antd.css';
 import './signin.css';
+import { FirebaseContext } from '../Firebase';
+import { Link, withRouter } from 'react-router-dom';
+
+import * as ROUTES from '../../constants/routes';
+
+const SignInPage = () => (
+    <div>
+    <h1>SignIn</h1>
+    <FirebaseContext.Consumer>
+    {firebase => <SignInForm firebase={firebase} />}
+    </FirebaseContext.Consumer>
+    </div>
+);
+
+class NormalLoginFormBase extends React.Component {
+    state = {
+        confirmDirty: false,
+        autoCompleteResult: [],
+        email: '',
+        password: '',
+        error: null,
+    };
+
+    handleSubmit = (e) => {
+        const { email, password } = this.state;
+
+        e.preventDefault();
+
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+
+        this.props.firebase
+        .doSignInWithEmailAndPassword(email, password)
+        .then(authUser => {
+            console.log('AUTH SUCCESSFUL');
+            this.props.history.push(ROUTES.HOME);
+
+        })
+        .catch(error => {
+            this.setState({ error });
+        });
 
 
+    }
 
-class NormalLoginForm extends React.Component {
+    onChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+        console.log('name', event.target.name)
+        console.log('onChange :)', event.target.value)
+    };
 
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        const {
+            autoCompleteResult,
+            email,
+            password,
+            error,
+        } = this.state;
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-
-    
-
-
-  }
-
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
-        <Form.Item>
-          {getFieldDecorator('userName', {
-            rules: [{ required: true, message: 'Please input your username!' }],
-          })(
-            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-          )}
-        </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-          )}
-        </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(
-            <Checkbox>Remember me</Checkbox>
-          )}
-          <a className="login-form-forgot" href="f">Forgot password</a>
-          <br />
-          <Button htmlType="submit" className="login-form-button">
+        return (
+            <Form onSubmit={this.handleSubmit} className="login-form">
+            <Form.Item>
+            {getFieldDecorator('userName', {
+                rules: [{ required: true, message: 'Please input your username!' }],
+            })(
+                <Input
+                prefix= {
+                    <Icon
+                    type="user"
+                    style= {
+                        {
+                            color: 'rgba(0,0,0,.25)'
+                        }
+                    }
+                    />
+                }
+                name="email"
+                value={password}
+                onChange={this.onChange.bind(this)}
+                placeholder="Username"
+                />
+            )}
+            </Form.Item>
+            <Form.Item>
+            {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+            })(
+                <Input
+                prefix = {
+                    <Icon
+                    type="lock"
+                    style= {
+                        {
+                            color: 'rgba(0,0,0,.25)'
+                        }
+                    }
+                    />
+                }
+                name="password"
+                type="password"
+                value={email}
+                onChange={this.onChange.bind(this)}
+                placeholder="Password"
+                />
+            )}
+            </Form.Item>
+            <Form.Item>
+            {getFieldDecorator('remember', {
+                valuePropName: 'checked',
+                initialValue: true,
+            })(
+                <Checkbox>Remember me</Checkbox>
+            )}
+            <a className="login-form-forgot" href="f">Forgot password</a>
+            <br />
+            <Button htmlType="submit" className="login-form-button">
             Log in
-          </Button>
-          <br />
-           Or <a href="signup">Register Now!</a>
-        </Form.Item>
-      </Form>
-    );
-  }
+            </Button>
+            <br />
+            Or <a href="signup">Register Now!</a>
+            </Form.Item>
+            </Form>
+        );
+    }
 }
 
-const SignIn = Form.create({ name: 'normal_login' })(NormalLoginForm);
+const SignInForm = withRouter(Form.create({ name: 'normal_login' })(NormalLoginFormBase));
 
-export default SignIn;
+export default SignInPage;
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {
-  Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete,
+    Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete,
 } from 'antd';
 
 import { FirebaseContext } from '../Firebase';
@@ -21,12 +21,12 @@ const INITIAL_STATE = {
 };
 
 const SignUpPage = () => (
-  <div>
+    <div>
     <h1>SignUp</h1>
     <FirebaseContext.Consumer>
-      {firebase => <SignUpForm firebase={firebase} />}
+    {firebase => <SignUpForm firebase={firebase} />}
     </FirebaseContext.Consumer>
-  </div>
+    </div>
 );
 
 class RegistrationFormBase extends React.Component {
@@ -36,166 +36,166 @@ class RegistrationFormBase extends React.Component {
         email: '',
         password: '',
         error: null,
-     };
+    };
 
 
-  handleSubmit = (e) => {
-    const { email, password } = this.state;
+    handleSubmit = (e) => {
+        const { email, password } = this.state;
 
-    e.preventDefault();
-    this.props.form.validateFieldsAndScroll((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-        email = values.email;
-        password = values.password;
-      }
-    });
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
 
-    this.props.firebase
-      .doCreateUserWithEmailAndPassword(email, password)
-      .then(authUser => {
-        this.props.history.push(ROUTES.HOME);
+        this.props.firebase
+        .doCreateUserWithEmailAndPassword(email, password)
+        .then(authUser => {
+            console.log('AUTH SUCCESSFUL');
+            this.props.history.push(ROUTES.HOME);
 
-      })
-      .catch(error => {
-        this.setState({ error });
-      });
+        })
+        .catch(error => {
+            this.setState({ error });
+        });
 
-      console.log("sdfsdfasdf", this);
+        console.log('email', email);
 
-
-
-
-  }
-
-  handleConfirmBlur = (e) => {
-    const value = e.target.value;
-    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
-  }
-
-  compareToFirstPassword = (rule, value, callback) => {
-    const form = this.props.form;
-    if (value && value !== form.getFieldValue('password')) {
-      callback('Two passwords that you enter is inconsistent!');
-    } else {
-      callback();
     }
-  }
 
-  validateToNextPassword = (rule, value, callback) => {
-    const form = this.props.form;
-    if (value && this.state.confirmDirty) {
-      form.validateFields(['confirm'], { force: true });
+    handleConfirmBlur = (e) => {
+        const value = e.target.value;
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
     }
-    callback();
-  }
 
-  onChange = (event) => {
-   this.setState({ [event.target.name]: event.target.value });
- };
+    compareToFirstPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('Two passwords that you enter is inconsistent!');
+        } else {
+            callback();
+        }
+    }
 
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    const {
-        autoCompleteResult,
-        email,
-        password,
-        error,
+    validateToNextPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['confirm'], { force: true });
+        }
+        callback();
+    }
+
+    onChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+        console.log('name', event.target.name)
+        console.log('onChange :)', event.target.value)
+    };
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        const {
+            autoCompleteResult,
+            email,
+            password,
+            error,
         } = this.state;
 
-    const formItemLayout = {
-      labelCol: {
-        xs: { span: 24 },
-        sm: { span: 8 },
-      },
-      wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 16 },
-      },
-    };
-    const tailFormItemLayout = {
-      wrapperCol: {
-        xs: {
-          span: 24,
-          offset: 0,
-        },
-        sm: {
-          span: 16,
-          offset: 8,
-        },
-      },
-    };
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 8 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 16 },
+            },
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0,
+                },
+                sm: {
+                    span: 16,
+                    offset: 8,
+                },
+            },
+        };
 
-    return (
-        <div>
-      <Form {...formItemLayout} onSubmit={this.handleSubmit}>
-        <Form.Item
-          label="E-mail"
-        >
-          {getFieldDecorator('email', {
-            rules: [{
-              type: 'email', message: 'The input is not valid E-mail!',
-            }, {
-              required: true, message: 'Please input your E-mail!',
-            }],
-          })(
-            <Input
+        return (
+            <div>
+            <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+            <Form.Item
+            label="E-mail"
+            >
+            {getFieldDecorator('email', {
+                rules: [{
+                    type: 'email', message: 'The input is not valid E-mail!',
+                }, {
+                    required: true, message: 'Please input your E-mail!',
+                }],
+            })(
+                <Input
+                name="email"
                 placeholder="Email"
                 value={email}
-                onChange={this.onChange}
-            />
-          )}
-        </Form.Item>
-        <Form.Item
-          label="Password"
-        >
-          {getFieldDecorator('password', {
-            rules: [{
-              required: true, message: 'Please input your password!',
-            }, {
-              validator: this.validateToNextPassword,
-            }],
-          })(
-            <Input
+                onChange={this.onChange.bind(this)}
+                />
+            )}
+            </Form.Item>
+            <Form.Item
+            label="Password"
+            >
+            {getFieldDecorator('password', {
+                rules: [{
+                    required: true, message: 'Please input your password!',
+                }, {
+                    validator: this.validateToNextPassword,
+                }],
+            })(
+                <Input
+                name="password"
                 type="password"
                 placeholder="Password"
                 value={password}
-                onChange={this.onChange}
-            />
-          )}
-        </Form.Item>
-        <Form.Item
-          label="Confirm Password"
-        >
-          {getFieldDecorator('confirm', {
-            rules: [{
-              required: true, message: 'Please confirm your password!',
-            }, {
-              validator: this.compareToFirstPassword,
-            }],
-          })(
-            <Input
+                onChange={this.onChange.bind(this)}
+                />
+            )}
+            </Form.Item>
+            <Form.Item
+            label="Confirm Password"
+            >
+            {getFieldDecorator('confirm', {
+                rules: [{
+                    required: true, message: 'Please confirm your password!',
+                }, {
+                    validator: this.compareToFirstPassword,
+                }],
+            })(
+                <Input
                 type="password"
                 placeholder="Confirm Password"
                 onBlur={this.handleConfirmBlur}
-            />
-          )}
-        </Form.Item>
+                />
+            )}
+            </Form.Item>
 
-        <Form.Item {...tailFormItemLayout}>
-          {getFieldDecorator('agreement', {
-            valuePropName: 'checked',
-          })(
-            <Checkbox>I have read the <a href="">agreement</a></Checkbox>
-          )}
-        </Form.Item>
-        <Form.Item {...tailFormItemLayout}>
-          <Button  htmlType="submit">Register</Button>
-        </Form.Item>
-      </Form>
-      </div>
-    );
-  }
+            <Form.Item {...tailFormItemLayout}>
+            {getFieldDecorator('agreement', {
+                valuePropName: 'checked',
+            })(
+                <Checkbox>I have read the <a href="">agreement</a></Checkbox>
+            )}
+            </Form.Item>
+            <Form.Item {...tailFormItemLayout}>
+            <Button  htmlType="submit">Register</Button>
+            </Form.Item>
+            </Form>
+            </div>
+        );
+    }
 }
 
 const SignUpForm = withRouter(Form.create({ name: 'register' })(RegistrationFormBase));
