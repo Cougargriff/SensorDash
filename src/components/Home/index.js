1f89b2cc33c5077aac0c2cbe2b713c67c31195c7
Griffin <cougargriff@gmail.com>
2019-06-14 00:27:20 -0700

    can display hr data!

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,30 +5,13 @@ import { connect } from 'react-redux'
 import { compose } from 'redux'
 import { firebaseConnect, isLoaded, isEmpty, firestoreConnect } from 'react-redux-firebase'
 
-import './maps.css';
+import './home.css';
 import GoogleMapReact from 'google-map-react';
 
 import { Charts, ChartContainer, ChartRow, YAxis, LineChart, Resizable } from "react-timeseries-charts";
 import { TimeSeries, TimeRange } from "pondjs";
+import { firestore } from 'firebase';
 
-
-// example data
-const data = {
-  name: "traffic",
-  columns: ["time", "value"],
-  points: [
-      [1, 1],
-      [2, 8],
-      [3, 27],
-      [4, 64],
-  ]
-};
-
-const series1 = new TimeSeries(data);
-
-var timerange = series1.timerange(); // range of times for given set
-// or
-// ... define your own ... = new TimeRange([begin, end]);
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
@@ -42,7 +25,7 @@ class Home extends Component {
   }
 
   componentDidMount() {
-    console.log("Home Mounted. props -> ", series1);
+    console.log("Home Mounted. props -> ", this);
   }
 
   render() {
@@ -50,13 +33,91 @@ class Home extends Component {
       return MapViewNoAuth();
     }
 
-    return MapViewAuth();
+    if(this.props.fire.data.triggers != null)
+    {
+      console.log("triggers loaded")
+    }
+
+    if(this.props.fire.data.location_history != null)
+    {
+      console.log("locations loaded")
+      
+    }
+
+    
+    else
+    {
+
+    }
+
+    
+
+    return MapViewAuth(this.props);
   }
 }
 
-const handleMapLoad = (map, maps) => {
-  console.log("Map Load Callback")
+const getHRSeries = (props) => {
+  var data = null;
+  if(props.fire.data.hr_data != null)
+  {
+    console.log("hr data loaded")
+    var hr_data = props.fire.data.hr_data
+    var p = [];
+    var keys = Object.keys(hr_data);
+
+    for(var i = 0; i < keys.length; i++)
+    {
+      var time = keys[i];
+      p.push([time * 1000, hr_data[time]])
+    }
+
+    data = {
+      name: "Heart Rates",
+      columns: ["time", "value"],
+      points: p
+    }
+
+    const series1 = new TimeSeries(data)
+  }
+  else {
+    data = {
+      name: "traffic",
+      columns: ["time", "value"],
+      points: [
+          [1, 0],
+          [2, 0],
+          [3, 0],
+          [4, 0],
+      ]
+    };
+  }
+
+  return new TimeSeries(data);
 }
+
+
+const getTimeStamp = () => {
+  var today = new Date()
+  var date = today.getFullYear() + '-' + ( (() => {
+
+      // inline utility function to get '0' before numbers in date format for month
+      // i.e. 1999-09-10 not 1999-9-10...
+      var month = today.getMonth() + 1
+      if(month < 10)
+      {
+        return "0" + month
+      }
+      else {
+        return month
+      }
+  })()) + '-' + (today.getDate());
+
+  return date;
+}
+const handleMapLoad = (map, maps, props) => {
+  // todo poly lines?
+}
+
 
 const MapViewNoAuth = () => (
   <div>
@@ -64,15 +125,15 @@ const MapViewNoAuth = () => (
   </div>
 );
 
-const MapViewAuth = () => (
+const MapViewAuth = (props) => (
 
   <div>
       <Resizable>
-        <ChartContainer timeRange={series1.timerange()}> 
+        <ChartContainer timeRange={getHRSeries(props).timerange()}> 
           <ChartRow height="500"> 
             <YAxis id="axis1" label="Y AXIS!" min={0} max={100} width="80" type="linear"/>
             <Charts>
-                <LineChart axis="axis1" series={series1}/>
+                <LineChart axis="axis1" series={getHRSeries(props)}/>
             </Charts>
           </ChartRow>
         </ChartContainer>
@@ -81,7 +142,7 @@ const MapViewAuth = () => (
       <GoogleMapReact
         bootstrapURLKeys={{ key:  "AIzaSyDM0FwQr5XwCYchZlrqV4eHYZBi6BEA3mo"}}
         yesIWantToUseGoogleMapApiInternals
-        onGoogleApiLoaded={ ({map, maps}) => handleMapLoad(map, maps) }
+        onGoogleApiLoaded={ ({map, maps, props}) => handleMapLoad(map, maps, props) }
         defaultCenter={{
           lat: 35.282753,
           lng: -120.659615
@@ -103,6 +164,7 @@ const MapViewAuth = () => (
 const mapStateToProps = state => {
   return { 
     auth: state.firebase.auth,
+    fire: state.firestore
   }
 }
 
@@ -113,5 +175,22 @@ const mapDispatchToProps = {
 export default compose(
   connect(mapStateToProps, mapDispatchToProps),
   firebaseConnect(),
-  firestoreConnect()
+  firestoreConnect(props => {
+      return [
+        {
+          collection: `users/${props.auth.uid}/triggers`,
+          storeAs: 'triggers'
+        },
+         {
+           collection: `users/${props.auth.uid}/location_history/${getTimeStamp()}/locations`,
+           storeAs: 'location_history'
+         },
+         {
+           collection: `users/${props.auth.uid}/hr_data/`,
+           doc: getTimeStamp(),
+           storeAs: 'hr_data'
+         }
+      ]
+    }
+  )
 )(Home);
